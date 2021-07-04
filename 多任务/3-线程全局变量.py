74e7f94264a9f25ceaebb1037f958f3c502eeb4d
z291384521 <43595612+z291384521@users.noreply.github.com>
2021-07-04 21:06:07 +0800

    锁的用法等

--- 多任务/3-线程全局变量.py
+++ 多任务/3-线程全局变量.py
@@ -1,3 +1,5 @@
+import threading
+import time
 '''
 在一个函数中对全局变量进行修改的时候，到底是否需要使用global进行说明
 要看是否对全局变量的执行指向进行了修改，
@@ -5,7 +7,7 @@
 如果，仅仅是修改了指向的空间中的数据，此时不用必须使用global
 
 '''
-#region 什么时候加golbe
+#region 什么时候加golbal
 num=100
 num2=[11,22]
 nub3=[77,88]
@@ -22,5 +24,27 @@ print("nub(%s), nub2(%s)"%(num,num2))
 test1()
 test2()
 print("nub(%s), nub2(%s)"%(num,num2))
+#endregion
+
+#region 多线程共享全局变量
+#定义一个全局变量
+g_num = 100
+#定义一个元组
+def add():
+    global g_num
+    g_num += 100
+    print("-----in add g_num=%d ----" % g_num)
+def show():
+    print("-----in add g_num=%d ----" % g_num)
+def  main():
+    t1 = threading.Thread(target=add)
+    t2 = threading.Thread(target=show)
+    t1.start()
+    time.sleep(1)
+    t2.start()
+    time.sleep(1)
+    print("-----in add g_num=%d ----" % g_num)
+if __name__ =="__main__":
+    main()
 #endregion
 
