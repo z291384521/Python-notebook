aaface6b5a7826e72ec6d82db4d746b13365c112
反转旋木 <291384521@qq.com>
2021-07-12 11:38:07 +0800

    迭代器实现与列表表达式

--- 多任务/协程/01-自己实现一个可以迭代的对象.py
+++ 多任务/协程/01-自己实现一个可以迭代的对象.py
@@ -3,7 +3,7 @@
 #isinstance([], Iterable)
 from collections import Iterator
 from collections import Iterable
-from time import *
+import time
 #创建一个函数 来实现迭代
 class Classmate:
     def __init__(self):
@@ -18,13 +18,21 @@ class Classmate:
         # 我们暂时忽略如何构造一个迭代器对象
         #看到报错创建返回值
         #return "这是一个返回值"（报错字符串不是字符串）
-        return ClassIterator()
+        return ClassIterator(self)
         pass
 class ClassIterator():
+    def __init__(self,obj):
+        self.obj = obj
+        self.current_num = 0
     def __iter__(self):
         pass
     def __next__(self):
-        pass
+        if self.current_num < len(self.obj.names):
+            ret = self.obj.names[self.current_num]
+            self.current_num += 1
+            return ret
+        else:
+            raise StopIteration
 classmate = Classmate()
 classmate.add("老王")
 classmate.add("王二")
@@ -35,6 +43,15 @@ classmate.add("张三")
 print(isinstance(classmate, Iterable))
 
 for name in classmate:
+    '''
+    for temp in xxxx_obj:
+        pass
+    运行逻辑
+    1.判断×xxx_obj是否是可以迭代
+    2.在第1步成立的前提现，调用iter函数得到xxxx_obj对象的_iter_方法的返回值
+    3._iter_方法的返回值是一个迭代器
+    4.调用next方法
+    '''
     print(name)
     time.sleep(1)
 #报错
