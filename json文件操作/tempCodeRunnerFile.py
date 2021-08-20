d666804f8338b1de9195c54b8b141c5bf14d12f0
z291384521 <43595612+z291384521@users.noreply.github.com>
2021-08-20 13:25:37 +0800

    0820 13

--- json文件操作/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import re
-newpak=open("2222.pak",'w',encoding='utf-8')
-with open("pak.txt", 'r', encoding="utf-8-sig") as load_r:
-    line = load_r.readline()
-    while line:
-        newname=line.lower()
-        newpak.write(newname)
-        line = load_r.readline()
---
+++ 元类/global参数.py
@@ -0,0 +1,7 @@
+a=1000
+def A():
+    pass
+class B():
+    pass
+C = B()
+print(globals())
