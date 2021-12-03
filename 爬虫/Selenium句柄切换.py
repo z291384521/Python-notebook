ed239e5bd43e0255571b5460deb7bea72bb24e67
反转旋木 <291384521@qq.com>
2021-12-03 16:42:23 +0800

    hhaaa

--- 爬虫/Selenium句柄切换.py
+++ 爬虫/Selenium句柄切换.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 import time
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
 # 1.创建浏览器对象
-driver = webdriver.Chrome(executable_path=r'爬虫\软件\chromedriver.exe')
+driver = webdriver.Chrome()
+
 # 2.操作浏览器对象
 # driver.get('https://www.baidu.com/')
 driver.get('https://www.aliyun.com/')
 current_windows = driver.window_handles
 print(current_windows)
-driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div[2]/div[2]/a[1]").click()
+driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div[1]/div[2]/div[2]/a[1]").click()
+# driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/div[2]/div[2]/a[1]").click()
 current_windows = driver.window_handles
 print(current_windows)
 #切换界面 要不然获得不了文档里面类容
@@ -15,7 +19,8 @@ driver.switch_to.window(current_windows[-1])
 # //*[@id="help-index-menu"]//a
 time.sleep(3)
 # drs = driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div/div[1]/ul//a")
-drs = driver.find_elements_by_xpath("/html/body/div[2]/div/div[3]/div/div[1]/ul//a")
+# drs = driver.find_elements_by_xpath("/html/body/div[2]/div/div[3]/div/div[1]/ul//a")
+drs = driver.find_elements(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/ul//a")
 # /html/body/div[2]/div/div[3]/div/div[1]/ul//a
 # drs = driver.find_element_by_xpath("//*[@id='start-from-here']/div[2]/ul/li[1]")
 for i in drs:
