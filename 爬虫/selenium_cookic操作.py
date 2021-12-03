7fb3ac76f938029833c14b4800c5b9192bdc697e
反转旋木 <291384521@qq.com>
2021-12-03 20:28:09 +0800

    120304

--- 爬虫/selenium_cookic操作.py
+++ 爬虫/selenium_cookic操作.py
@@ -4,5 +4,12 @@ url = "http://www.baidu.com"
 driver = webdriver.Chrome()
 driver.get(url)
 print(driver.get_cookies())
+#进行cookic处理
+#变成BIDUPSID=F517CB2609DDE4009BD6C57C7115E6C0;
+# PSTM=1638524103; BAIDUID=F517CB2609DDE400638C14DC5B26CF7E:FG=1;
+# BD_HOME=1;
+# H_PS_PSSID=35104_31660_34584_34505_35234_34578_35329_35324_26350_35301;
+# BD_UPN=12314753;
+# BA_HECTOR=282124a424212k45ik1gqjpa90r
 cookies = {data['name']: data['value']for data in driver.get_cookies()}
 print(cookies)
