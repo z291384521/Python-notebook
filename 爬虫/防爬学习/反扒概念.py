81cac719dbb6b7b13b82b8656c6e0ff0d17b8040
反转旋木 <291384521@qq.com>
2021-12-06 16:58:36 +0800

    反爬教程

---
+++ 爬虫/防爬学习/反扒概念.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/12/06 16:11
+# @Author  : ZRQ
+# @Email   : 291384521@qq.com
+# @File    : 反扒概念.py
+# @Software: PyCharm
+# PV是什么 每次点击刷新就会产生PV
+# - 基于身份识别进行反爬
+# - 基于爬虫行为进行反爬
+# - 基于数据加密进行反爬
