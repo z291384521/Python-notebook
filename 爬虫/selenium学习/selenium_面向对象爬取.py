f9ce63989a77fb8d5c47a18e5f23bd4d1a1b4da4
反转旋木 <291384521@qq.com>
2021-12-09 13:50:40 +0800

    996信息爬虫提交

--- 爬虫/selenium学习/selenium_面向对象爬取.py
+++ 爬虫/selenium学习/selenium_面向对象爬取.py
@@ -4,12 +4,17 @@
 # @Email   : 291384521@qq.com
 # @File    : selenium_面向对象爬取.py.py
 # @Software: PyCharm
+
+
+
+
 from selenium import webdriver
 import time
 class Douyu(object):
     def __init__(self):
         self.url = 'https://www.douyu.com/directory/all'
         self.driver = webdriver.Chrome()
+
     def parse_data(self):
         pass
     def run(self):
