04625c3a2e582b995a5a0ed3f3d156601598f131
反转旋木 <291384521@qq.com>
2021-12-09 23:09:01 +0800

    996信息爬虫提交

--- 爬虫/有道翻译.py
+++ 爬虫/有道翻译.py
@@ -8,6 +8,8 @@ import json
 url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
 
 def get_md5(value):
+    '''
+    '''
     md5 = hashlib.md5()
     md5.update(value.encode('utf-8'))
     return md5.hexdigest()
