dc63625199f122d905891ea769742379c4c5eb7c
反转旋木 <291384521@qq.com>
2022-02-11 19:02:55 +0800

    完善git

--- git自动更新模块/deploy/config.py
+++ git自动更新模块/deploy/config.py
@@ -5,7 +5,7 @@
 #@Date         : 2022-02-10 16:16:49
 #@FilePath     : \Python-notebook\git自动更新模块\deploy\config.py
 #@Email        : 291384521@qq.com
-#@LastEditTime : 2022-02-11 18:10:26
+#@LastEditTime : 2022-02-11 19:01:49
 
 
 import copy
@@ -78,3 +78,34 @@ class DeployConfig:
         if value == 'null' or value == 'false' or value == '':
             return False
         return True
+    def execute(self, command, allow_failure=False):
+        """
+        Args:
+            command (str):
+            allow_failure (bool):
+
+        Returns:
+            bool: If success.
+                Terminate installation if failed to execute and not allow_failure.
+        """
+        command = command.replace(r'\\', '/').replace('\\', '/').replace('\"', '"')
+        print(command)
+        error_code = os.system(command)
+        if error_code:
+            if allow_failure:
+                print(f'[ allowed failure ], error_code: {error_code}')
+                return False
+            else:
+                print(f'[ failure ], error_code: {error_code}')
+                self.show_error()
+                raise ExecutionError
+        else:
+            print(f'[ success ]')
+            return True
+
+    def show_error(self):
+        self.show_config()
+        print('')
+        hr1('Update failed')
+        print('Please check your deploy settings in config/deploy.yaml '
+              'and re-open Alas.exe')        
