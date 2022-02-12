cf0b19124e187ea12f17d08c3720fb5b2e3e47b3
反转旋木 <291384521@qq.com>
2022-02-12 17:59:20 +0800

    updater未完成

--- git自动更新模块/webui/updater.py
+++ git自动更新模块/webui/updater.py
@@ -5,7 +5,9 @@
 #@Date         : 2022-02-12 09:54:26
 #@FilePath     : \Python-notebook\git自动更新模块\webui\updater.py
 #@Email        : 291384521@qq.com
-#@LastEditTime : 2022-02-12 11:17:07
+#@LastEditTime : 2022-02-12 16:50:59
+import datetime
+import threading
 from deploy.git import GitManager
 from deploy.pip import PipManager
 from deploy.config import DeployConfig, ExecutionError
@@ -48,3 +50,29 @@ class Updater(Config,GitManager,PipManager):
         super().__init__(file)
         #状态码先赋值为0
         self.state= 0
+        self.event: threading.Event = None
+    @property
+    def delay(self):
+        self.read()
+        #延迟多少分钟后检查下是不是最新的
+        return int(self.config['CheckUpdateInterval'])*60
+
+    @property
+    def schedule_time(self):
+        self.read()
+        #自动更新时间
+        t = self.config['AutoRestartTime']
+        if t != '':
+            #返回更新事件
+            return datetime.time.fromisoformat(t)
+        else:
+            return None    
+
+    @cached_property
+    def repo(self):
+        return self.config['Repository']
+
+    @cached_property
+    def branch(self):
+        return self.config['Branch']
+    
