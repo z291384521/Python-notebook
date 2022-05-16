68eb8084b4d0f6700f4525e908a6065ae2ae8de4
反转旋木 <291384521@qq.com>
2022-05-16 16:42:38 +0800

    仓库删除了模块 移交到其他地方

---
+++ python一些模块/0-语法知识/os获得上层目录.py
@@ -0,0 +1,4 @@
+#..上级
+save_path=os.path.abspath(os.path.join(input_path, ".."))
+#上上级
+save_path=os.path.abspath(os.path.join(input_path, "../.."))
---
+++ python一些模块/0-语法知识/os获得文件的路径与名字.py
@@ -0,0 +1,12 @@
+# 返回文件名
+# os.path.basename(path)	
+# os.path.dirname(path)	返回文件路径
+
+
+
+# import os
+# print(os.path.basename("C:/Users/top/PycharmProjects/untitled4/venv/a_4.py"))
+
+# 上面代码输出为：
+
+# a_4.py
