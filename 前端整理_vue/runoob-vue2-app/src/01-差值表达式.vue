ec846660103f57e5a55445f5c67004dd66134049
z291384521 <43595612+z291384521@users.noreply.github.com>
2022-08-27 22:14:53 +0800

    慢慢来

---
+++ 前端整理_vue/runoob-vue2-app/src/01-差值表达式.vue
@@ -0,0 +1,30 @@
+<!-- <vue 根据提示回车即可出现如下结构 -->
+<template>
+    <div>
+        <!-- 2. 把值赋予到标签 -->
+        <h1>{{ msg }}</h1>
+        <h2>{{ obj.name }}</h2>
+        <h3>{{ obj.age >= 18 ? '成年了' : '未成年' }}</h3>
+        <a v-bind:href="url">点击去百度</a>
+    </div>
+</template>
+
+<script>
+export default {
+    // 1. 变量在data函数return的对象上
+    data() {
+        return {
+            url: 'http://www.baidu.com',
+            msg: 'Hello, Vue',
+            obj: {
+                name: '小vue',
+                age: 5
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
