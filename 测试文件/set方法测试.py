414f84ee1647e2034cca0f9b49f8a9b5e087230d
反转旋木 <qiuye@fanzhuanxuanmudeMacBook-Pro.local>
2023-02-20 22:43:39 +0800

    再次整理开始启用

--- 测试文件/set方法测试.py
+++
@@ -1,4 +0,0 @@
-from  set魔法方法测试 import Fun 
-
-fun = Fun() 
-fun.test()
--- 测试文件/测试正则.py
+++
@@ -1,6 +0,0 @@
-#/add/000007.html
-import re
-
-
-ret = re.match(r"/add/(\d+)\.html","/add/000007.html")
-print(ret.group(1))
