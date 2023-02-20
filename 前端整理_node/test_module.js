6f63671feb202da093484537896589ab8b141b2c
反转旋木 <qiuye@fanzhuanxuanmudeMacBook-Pro.local>
2023-02-20 22:42:27 +0800

    再次整理开始启用

--- 前端整理_node/test_module.js
+++
@@ -1,5 +0,0 @@
-// require("./dataFormat")
-const moment = require('moment')
-
-const dt = moment().format('YYYY-MM-DD HH:mm:ss')
-console.log(dt)
--- 前端整理_vue/runoob-vue2-app/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  devServer: {
-    port: 3000,
-    open: true
-  },
-  lintOnSave: false, // 关闭eslint检查
-  transpileDependencies: true
-})
